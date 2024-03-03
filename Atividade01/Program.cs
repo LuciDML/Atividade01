57eb36f895d241049bb881a15c931000c07d8f6f
luciano <luciano@blogudev.com>
2024-03-03 15:17:20 -0300

    Primeiros arquivos

---
+++ Atividade01/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Atividade01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
